4d810265d98b7779bddc3505113db25537571603
Welberrr <welberhenrique.costa@gmail.com>
2025-09-21 00:41:38 -0300

    Feat: Estrutura inicial

---
+++ src/main/java/welber/stf/basicAuth_to_bearerToken/BasicAuthToBearerTokenApplication.java
@@ -0,0 +1,13 @@
+package welber.stf.basicAuth_to_bearerToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthToBearerTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthToBearerTokenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/welber/stf/basicAuth_to_bearerToken/BasicAuthToBearerTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package welber.stf.basicAuth_to_bearerToken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthToBearerTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
