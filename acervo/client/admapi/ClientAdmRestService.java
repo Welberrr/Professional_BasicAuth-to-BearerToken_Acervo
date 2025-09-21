7f0ae346c5c915ea439add9c653a2e3a68f353e4
Welberrr <welberhenrique.costa@gmail.com>
2025-09-21 00:54:03 -0300

    Feat: implementando interface ClientAdm

--- acervo/client/admapi/ClientAdmRestService.java
+++ acervo/client/admapi/ClientAdmRestService.java
@@ -1,5 +1,18 @@
-package acervo.client.admapi;
+package br.jus.stf.acervo.client.admapi;
+
+import br.jus.stf.acervo.model.dto.SetorDto;
+import br.jus.stf.acervo.model.dto.UsuarioDto;
+import br.jus.stf.acervo.model.dto.UsuarioSimplesDto;
+
+import java.util.List;
 
 public interface ClientAdmRestService {
 
+    SetorDto obtemSetor(Long id);
+    List<SetorDto> listaTodosSetores();
+    List<SetorDto> listaGabinetesMinistros();
+
+    UsuarioDto obtemUsuario(String id);
+
+    List<UsuarioSimplesDto> listaTodosUsuariosSimples();
 }
